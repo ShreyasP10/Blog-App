7c967ad07e15cc0d15e54e089c035df0f9516550
Shreyas Pawar <shreyaspawar1011@gmail.com>
2025-05-15 19:30:47 +0530

    Update MainActivity.kt

--- app/src/main/java/com/shreyaspawar/blogapp/MainActivity.kt
+++ app/src/main/java/com/shreyaspawar/blogapp/MainActivity.kt
@@ -15,7 +15,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-
+ 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
